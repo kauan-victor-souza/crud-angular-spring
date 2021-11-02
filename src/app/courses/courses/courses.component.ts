e948e7692bf165a71ff08fd218448c6ca49b463d
Kauan Souza <k1one.g.vitor@gmail.com>
2021-11-02 16:35:32 -0300

    Project Angular Spring

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,26 @@
+import { CoursesService } from './../services/courses.service';
+import { Course } from './../model/course';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name','category'];
+
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    //this.courses = [];
+    //this.CoursesService = new CoursesService();
+    this.courses$ = this.coursesService.list();
+  }
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/courses/model/course.ts
@@ -0,0 +1,8 @@
+import { LocationChangeEvent } from "@angular/common";
+
+export interface Course {
+  _id:      string;
+  name:     string;
+  category: string;
+
+}
